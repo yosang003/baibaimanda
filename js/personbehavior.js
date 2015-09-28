934110f8372fd2970d5b14f9356ea211651ad17e
yosang003 <1154755167@qq.com>
2015-09-28 23:11:41 +0800

    yxz

--- js/personbehavior.js
+++ js/personbehavior.js
@@ -9,7 +9,11 @@ $(function($){
 })
 
 function initPage(){
-	    //当前登录者的数据
+	// ------这里是前端设置的jsessionid的值，这个cookie应该由后台设置，这里是模拟
+	setcookie("jsessionid","1");
+	// -----------------------------
+	
+	var mycookies=getcookie("jsessionid");
 	    $.ajax({
 			url:'testjson/indexreturn.json',
 			type:'GET',
@@ -26,3 +30,18 @@ function initPage(){
 		
 
 	}
+
+	//前端设置一个cookie
+	function setcookie(name,value)
+	{
+		var cookie=name+"="+encodeURIComponent(value);
+		document.cookie=cookie;
+	}
+	//前端获取cookie的值
+	 function getcookie(objname){//获取指定名称的cookie的值
+          var arrstr = document.cookie.split("; ");
+          for(var i = 0;i < arrstr.length;i ++){
+          var temp = arrstr[i].split("=");
+          if(temp[0] == objname) return unescape(temp[1]);
+      }
+}
